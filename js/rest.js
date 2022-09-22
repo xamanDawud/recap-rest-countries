9c9916d55e3bd8091eafeec36cebf0bf63ff5db5
xamanDawud <ahmeddaudking100@gmail.com>
2022-09-22 22:34:59 +0600

    destructuring two way explored

--- js/rest.js
+++ js/rest.js
@@ -18,15 +18,25 @@ const getCountry = (countries) => {
   });
 };
 
-const displayCountry = (country) => {
-  //   console.log(country);
-  //   console.log(country.name.common);
-  const { name, area, flags, languages, region } = country;
+// -------------Original Destructuring----------------
+
+// const displayCountry = (country) => {
+//   const { name, area, flags, languages, region } = country;
+//   return `
+//     <h1>${name.common}</h1>
+//         <img src='${flags.png}'>
+//         <h2>Area:${area}</h2>
+//         <h2>Region: ${region}</h2>
+//     `;
+// };
+
+// --------------Destructuring second system-----------------
+const displayCountry = ({ name, area, flags, region }) => {
   return `
-        <img src='${flags.png}'>
-        <h1>${name.common}</h1>
-        <h2>Area:${area}</h2>
-        <h2>Region: ${region}</h2>
-    `;
-};
+      <h1>${name.common}</h1>
+          <img src='${flags.png}'>
+          <h2>Area:${area}</h2>
+          <h2>Region: ${region}</h2>
+      `;
+}; 
 loadCountries();
